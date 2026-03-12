79aa24e046f7d6cda756904215fc0cb4f6b798f1
Piyusha Shewale <piyushashewale0@gmail.com>
2026-03-12 16:53:19 +0530

    Added optimal solution for k rotation of an array

--- Arrays/KrotateArray.java
+++ Arrays/KrotateArray.java
@@ -1,22 +1,23 @@
 import java.util.Arrays;
 class Solution{
-    public void RotateArray(int[] arr,int k){
-        k=k%arr.length;
-        int[] temp= new int[k];
-        for(int i=0;i<k;i++){
-            temp[i]=arr[i];
-        }
-        for(int i=k;i<arr.length;i++){
-            arr[i-k]=arr[i];
-        }
-        for(int i= 0;i<k;i++){
-            arr[arr.length-k+i]=temp[i];
+    public void rotateArray(int[] arr,int k){
+        reverse(arr,0,k-1);
+        reverse(arr,k,arr.length-1);
+        reverse(arr,0,arr.length-1);
+    }
+    public void reverse(int[] arr,int left,int right){
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
         }
     }
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5,6,7};
+        int[] arr= {1,2,3,4,5,6,7};
         Solution obj= new Solution();
-        obj.RotateArray(arr,3);
+        obj.rotateArray(arr,3);
         System.out.print(Arrays.toString(arr));
     }
 }
