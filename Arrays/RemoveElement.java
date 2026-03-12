8e0ad1c2db92217fa9401bb674c7d880236ed087
Piyusha Shewale <piyushashewale0@gmail.com>
2026-03-12 12:58:56 +0530

    Added solution for removing an element from array

---
+++ Arrays/RemoveElement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        for(int j=0 ;j<nums.length;j++){
+            if(arr[i]==val ||)
+        }
+    }
+    public static void main(String[] args){
+        int[] nums={3,2,2,3};
+        Solution obj= new Solution();
+        System.out.println(obj.removeElement(nums,3));
+        System.out.print(Arrays.toString(nums));
+    }
+}
